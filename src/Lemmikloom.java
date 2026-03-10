87616c9c6a6bc263eaf573bfd91cd4225d696678
0lder0 <liliiannolder@gmail.com>
2026-03-10 13:54:45 +0200

    projekti algus

---
+++ src/Lemmikloom.java
@@ -0,0 +1,80 @@
+public class Lemmikloom {
+    private String nimi;
+    private String liik;
+    private int vanus;
+    private int tervis = 50;
+    private int nälg = 50;
+    private int puhtus = 50;
+    private int meeleolu = 50;
+    private boolean surmaSeisund = false;
+
+    public Lemmikloom(String nimi, String liik, int vanus) {
+        this.nimi = nimi;
+        this.liik = liik;
+        this.vanus = vanus;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public String getLiik() {
+        return liik;
+    }
+
+    public int getVanus() {
+        return vanus;
+    }
+
+    public int getTervis() {
+        return tervis;
+    }
+
+    public int getNälg() {
+        return nälg;
+    }
+
+    public int getPuhtus() {
+        return puhtus;
+    }
+
+    public int getMeeleolu() {
+        return meeleolu;
+    }
+
+    public boolean isSurmaSeisund() {
+        return surmaSeisund;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+    public void setLiik(String liik) {
+        this.liik = liik;
+    }
+
+    public void setVanus(int vanus) {
+        this.vanus = vanus;
+    }
+
+    public void setTervis(int tervis) {
+        this.tervis = tervis;
+    }
+
+    public void setNälg(int nälg) {
+        this.nälg = nälg;
+    }
+
+    public void setPuhtus(int puhtus) {
+        this.puhtus = puhtus;
+    }
+
+    public void setMeeleolu(int meeleolu) {
+        this.meeleolu = meeleolu;
+    }
+
+    public void setSurmaSeisund(boolean surmaSeisund) {
+        this.surmaSeisund = surmaSeisund;
+    }
+}
